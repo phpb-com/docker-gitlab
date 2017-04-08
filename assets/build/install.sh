923bc6a1159ba1a149fe6f153b7a816d7c6e5c88
Ian Matyssik <ian@phpb.com>
2017-04-08 14:50:10 +0900

    Add libsqlite3-dev to build dependency

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -13,7 +13,7 @@ GITLAB_MONITOR_CLONE_URL=https://gitlab.com/gitlab-org/gitlab-monitor.git
 GEM_CACHE_DIR="${GITLAB_BUILD_DIR}/cache"
 
 BUILD_DEPENDENCIES="gcc g++ make patch pkg-config cmake paxctl \
-  libc6-dev ruby${RUBY_VERSION}-dev libkrb5-dev \
+  libc6-dev ruby${RUBY_VERSION}-dev libkrb5-dev libsqlite3-dev \
   libmysqlclient-dev libpq-dev zlib1g-dev libyaml-dev libssl-dev \
   libgdbm-dev libreadline-dev libncurses5-dev libffi-dev \
   libxml2-dev libxslt-dev libcurl4-openssl-dev libicu-dev"
