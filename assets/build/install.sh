be2f86dc3885eac4f2f5dfeb59c6ea0bdc674cf8
Ian Matyssik <ian@phpb.com>
2017-04-09 16:32:59 +0900

    Delete .cache and .bundle in gitlab home directory

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -518,3 +518,5 @@ echo "Removing documentation..." >&2
 find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true
 find /usr/share/doc -empty|xargs rmdir || true
 rm -rf /usr/share/man /usr/share/groff /usr/share/info /usr/share/lintian /usr/share/linda /var/cache/man
+rm -rf ${GITLAB_HOME}/.cache
+rm -rf ${GITLAB_HOME}/.bundle
