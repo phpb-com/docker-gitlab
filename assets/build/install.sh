beb6e8b3ca93bb054f30952ca427bf8c8b192b4a
Ian Matyssik <ian@phpb.com>
2017-04-28 22:13:03 +0900

    Add GITLAB_WORKHORSE_SENTRY_DSN parameter to configure sentry DSN for workhorse

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -401,7 +401,7 @@ cat > /etc/supervisor/conf.d/gitlab-workhorse.conf <<EOF
 [program:gitlab-workhorse]
 priority=20
 directory=${GITLAB_INSTALL_DIR}
-environment=HOME=${GITLAB_HOME}
+environment=HOME=${GITLAB_HOME},GITLAB_WORKHORSE_SENTRY_DSN="{{GITLAB_WORKHORSE_SENTRY_DSN}}"
 command=/usr/local/bin/gitlab-workhorse
   -listenUmask 0
   -listenNetwork tcp
