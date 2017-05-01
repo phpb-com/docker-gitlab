06ffaf0ecfa3f28bb7ebd9a44bf75788c0d5518d
Ian Matyssik <ian@phpb.com>
2017-05-02 02:56:10 +0900

    Make git limits within range

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -47,11 +47,11 @@ exec_as_git git config --global gc.auto 0
 exec_as_git git config --global repack.writeBitmaps true
 
 # Increase git limits to allow for large imports
-exec_as_git git config --global core.packedGitLimit 1024m
-exec_as_git git config --global core.packedGitWindowSize 1024m
-exec_as_git git config --global pack.deltaCacheSize 4096m
-exec_as_git git config --global pack.packSizeLimit 4096m
-exec_as_git git config --global pack.windowMemory 4096m
+exec_as_git git config --global core.packedGitLimit 512m
+exec_as_git git config --global core.packedGitWindowSize 512m
+exec_as_git git config --global pack.deltaCacheSize 2047m
+exec_as_git git config --global pack.packSizeLimit 2047m
+exec_as_git git config --global pack.windowMemory 2047m
 exec_as_git git config --global http.postBuffer 1048576000
 
 #
